ef0aefa477f6156cc45f23b71552e7b519928c88
chanhee <chanhee.jean22@gmail.com>
2021-01-29 10:08:54 +0900

    make_conflict

--- test.py
+++ test.py
@@ -3,12 +3,9 @@
 
 if __name__ == "__main__":
     print("github tutorial")
-<<<<<<< HEAD
     print("hahaha") 
     for i in range(100000):
         print("I dont understand how to rebase")
-        
-=======
-    print("haahaha");
+        print("haahaha");
     print("hahaha")
->>>>>>> 3281b16240dc18a5142d6059e7ebebbc625b466d
+ 
